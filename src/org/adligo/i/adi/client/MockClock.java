94aa64515bd81b793d39677edc0b89cb90a714ab
scott <scott@adligo.com>
2009-10-31 07:33:08 +0000

    removed I_Clock added MockClock

---
+++ src/org/adligo/i/adi/client/MockClock.java
@@ -0,0 +1,18 @@
+package org.adligo.i.adi.client;
+
+public class MockClock implements I_Invoker {
+	private static long time;
+
+	@Override
+	public Object invoke(Object valueObject) {
+		return time;
+	}
+
+	public static void setTime(long p_time) {
+		time = p_time;
+	}
+	
+	public static long getTime() {
+		return time;
+	}
+}
