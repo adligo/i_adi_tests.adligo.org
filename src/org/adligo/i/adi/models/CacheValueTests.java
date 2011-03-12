7346f5053e18d8f040ccd83aa490f5bc8f7e2287
scott <scott@adligo.com>
2011-03-12 10:41:17 +0000

    updated for writes to Cache in the same millisecond

--- src/org/adligo/i/adi/models/CacheValueTests.java
+++ src/org/adligo/i/adi/models/CacheValueTests.java
@@ -5,26 +5,32 @@ import org.adligo.tests.ATest;
 
 public class CacheValueTests extends ATest {
 
+	public void testToString() {
+		CacheValue cv = new CacheValue("a", 0, "hey");
+		assertEquals("CacheValue [fullPath=a,putTime=0,value=hey]",cv.toString());
+		
+	}
+	
 	public void testTimeCrunchString() {
 		CacheValue cv = new CacheValue("", 0, "hey");
-		assertEquals("/0/0",cv.getTimeCrunchString());
+		assertEquals("/0/0@" + cv.hashCode(),cv.getTimeCrunchString());
 		
 		cv = new CacheValue("",1000, "hey");
-		assertEquals("/0/1000",cv.getTimeCrunchString());
+		assertEquals("/0/1000@" + cv.hashCode(),cv.getTimeCrunchString());
 		
 		cv = new CacheValue("",59999, "hey");
-		assertEquals("/0/59999",cv.getTimeCrunchString());
+		assertEquals("/0/59999@" + cv.hashCode(),cv.getTimeCrunchString());
 		
 		cv = new CacheValue("",60000, "hey");
-		assertEquals("/1/60000",cv.getTimeCrunchString());
+		assertEquals("/1/60000@" + cv.hashCode(),cv.getTimeCrunchString());
 		
 		cv = new CacheValue("",-1, "hey");
-		assertEquals("/0/-1",cv.getTimeCrunchString());
+		assertEquals("/0/-1@" + cv.hashCode(),cv.getTimeCrunchString());
 		
 		cv = new CacheValue("",-59999, "hey");
-		assertEquals("/0/-59999",cv.getTimeCrunchString());
+		assertEquals("/0/-59999@" + cv.hashCode(),cv.getTimeCrunchString());
 		
 		cv = new CacheValue("",-60000, "hey");
-		assertEquals("/-1/-60000",cv.getTimeCrunchString());
+		assertEquals("/-1/-60000@" + cv.hashCode(),cv.getTimeCrunchString());
 	}
 }
