fe7f7f663bceae805e7e3bafdfb0430cafd16822
scott <scott@adligo.com>
2011-03-12 09:42:10 +0000

    updates

--- src/org/adligo/i/adi/light/MemoryTests.java
+++ src/org/adligo/i/adi/light/MemoryTests.java
@@ -2,6 +2,7 @@ package org.adligo.i.adi.light;
 
 import org.adligo.i.adi.client.light.MemoryReader;
 import org.adligo.i.adi.client.light.MemoryWriter;
+import org.adligo.i.adi.client.models.MemoryValue;
 import org.adligo.i.adi.client.models.MemoryWriterToken;
 import org.adligo.tests.ATest;
 
@@ -62,5 +63,50 @@ public class MemoryTests extends ATest {
 		assertTrue(result);
 		obj = reader.invoke("hey");
 		assertNull(obj);
+		
+		Object firstOwner = new Object();
+		token.setValue("owned");
+		token.setOwner(firstOwner);
+		result = (Boolean) writer.invoke(token);
+		assertTrue(result);
+		assertEquals("owned", reader.invoke("hey"));
+		
+		token.setOwner(null);
+		caught = null;
+		try {
+			writer.invoke(token);
+		} catch (Exception x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertTrue(caught instanceof IllegalArgumentException);
+		assertEquals(MemoryWriter.ONLY_THE_OWNER_OF_A_MEMORY_VALUE_IS_ALLOWED_TO_MODIFY_IT
+				+ token,
+				caught.getMessage());
+		
+		token.setOwner(new Object());
+		caught = null;
+		try {
+			writer.invoke(token);
+		} catch (Exception x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertTrue(caught instanceof IllegalArgumentException);
+		assertEquals(MemoryWriter.ONLY_THE_OWNER_OF_A_MEMORY_VALUE_IS_ALLOWED_TO_MODIFY_IT
+				+ token,
+				caught.getMessage());
+		
+		token.setValue("owned2");
+		token.setOwner(firstOwner);
+		result = (Boolean) writer.invoke(token);
+		assertTrue(result);
+		assertEquals("owned2", reader.invoke("hey"));
+		
+		
+		token.setValue(null);
+		result = (Boolean) writer.invoke(token);
+		assertTrue(result);
+		assertNull(reader.invoke("hey"));
 	}
 }
---
+++ src/org/adligo/i/adi/models/MemoryWriterTokenTests.java
@@ -0,0 +1,21 @@
+package org.adligo.i.adi.models;
+
+import org.adligo.i.adi.client.models.MemoryWriterToken;
+import org.adligo.tests.ATest;
+
+public class MemoryWriterTokenTests extends ATest {
+
+	public void testToString() {
+		MemoryWriterToken token = new MemoryWriterToken();
+		
+		assertEquals("MemoryWriterToken [key=null,value=null,owner=null]", token.toString());
+		token.setKey("a");
+		assertEquals("MemoryWriterToken [key=a,value=null,owner=null]", token.toString());
+		token.setValue("b");
+		assertEquals("MemoryWriterToken [key=a,value=b,owner=null]", token.toString());
+		token.setOwner("c");
+		assertEquals("MemoryWriterToken [key=a,value=b,owner=c]", token.toString());
+		
+	
+	}
+}
